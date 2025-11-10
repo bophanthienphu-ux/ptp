9ded261de9e8376f0d43c7b42b1c3b046e1dd3a7
phu <bophanthienphu@gmail.com>
2025-11-10 21:41:47 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -7,12 +7,34 @@ const express = require('express');
             const axios = require('axios');
             const fs = require('fs');
             const path = require('path');
+            const YTDlpWrap = require('yt-dlp-wrap').default;
         const PORT = 3000; // You can choose any available port
 
         // Define a simple route for the root URL
         app.get('/', (req, res) => {
-            exec('python3 -m pip install -U "yt-dlp[default]"',(stdout)=>{
-                res.send(`${stdout}`)
+            let ytDlpEventEmitter = ytDlpWrap
+    .exec([
+        'https://www.youtube.com/watch?v=aqz-KE-bpKQ',
+        '-f',
+        'best',
+        '-o',
+        'output.mp4',
+    ])
+    .on('progress', (progress) =>
+        console.log(
+            progress.percent,
+            progress.totalSize,
+            progress.currentSpeed,
+            progress.eta
+        )
+    )
+    .on('ytDlpEvent', (eventType, eventData) =>
+        console.log(eventType, eventData)
+    )
+    .on('error', (error) => console.error(error))
+    .on('close', () => console.log('all done'));
+
+res.status(200).send(ytDlpEventEmitter.ytDlpProcess.pid);
             })
         });
 
