08a0ddab1c9580e598bd6150ea9509e92a676195
phu <bophanthienphu@gmail.com>
2025-11-29 18:09:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.get('/vid', (req, res) => {
         // Đảm bảo codec là H.264/AAC cho MP4 tương thích
         .videoCodec('libx264')
         .audioCodec('aac')
+        .outputOptions(['-crf 50'])
         .on('end', () => {
             console.log(`Chuyển đổi hoàn tất: ${outputPath}`);
             
